471e92e670ec3654cc01510f9efc002969f5fe67
RedWriter607 <61717394+redwriter607@users.noreply.github.com>
2024-04-26 18:10:03 -0400

    Basic database access
    pokedex.php sucessfully grabs and prints an unformatted table of all database columns EXCEPT Form and Generation, since those are not meaningful with only generation 1 Pokémon.

--- pokedex.php
+++ pokedex.php
@@ -1,83 +1,57 @@
 <?php
 require_once('database.php');
 
-// Get category ID
-$category_id = filter_input(INPUT_GET, 'category_id', FILTER_VALIDATE_INT);
-if ($category_id == NULL || $category_id == FALSE) {
-    $category_id = 1;
-}
-
-// Get name for selected category
-$queryCategory = 'SELECT * FROM categories
-                  WHERE categoryID = :category_id';
-$statement1 = $db->prepare($queryCategory);
-$statement1->bindValue(':category_id', $category_id);
-$statement1->execute();
-$category = $statement1->fetch();
-$category_name = $category['categoryName'];
-$statement1->closeCursor();
-
-// Get all categories
-$queryAllCategories = 'SELECT * FROM categories
-                       ORDER BY categoryID';
-$statement2 = $db->prepare($queryAllCategories);
-$statement2->execute();
-$categories = $statement2->fetchAll();
-$statement2->closeCursor();
-
-// Get products for selected category
-$queryProducts = 'SELECT * FROM products
-                  WHERE categoryID = :category_id
-                  ORDER BY productID';
-$statement3 = $db->prepare($queryProducts);
-$statement3->bindValue(':category_id', $category_id);
-$statement3->execute();
-$products = $statement3->fetchAll();
-$statement3->closeCursor();
+// Get all Pokémon
+$queryPokedex = 'SELECT * FROM Pokedex
+                  ORDER BY id';
+$statement = $db->prepare($queryPokedex);
+$statement->execute();
+$Pokedex = $statement->fetchAll();
+$statement->closeCursor();
 ?>
 <!DOCTYPE html>
 <html>
 <!-- the head section -->
 <head>
-    <title>My Guitar Shop</title>
+    <title>Pokédex</title>
     <link rel="stylesheet" href="main.css" />
 </head>
 
 <!-- the body section -->
 <body>
 <main>
-    <h1>Product List</h1>
-    <aside>
-        <!-- display a list of categories -->
-        <h2>Categories</h2>
-        <nav>
-        <ul>
-            <?php foreach ($categories as $category) : ?>
-            <li>
-                <a href="?category_id=<?php echo $category['categoryID']; ?>">
-                    <?php echo $category['categoryName']; ?>
-                </a>
-            </li>
-            <?php endforeach; ?>
-        </ul>
-        </nav>
-    </aside>
 
     <section>
-        <!-- display a table of products -->
-        <h2><?php echo $category_name; ?></h2>
+        <!-- display a table of all Pokémon -->
+        <h2><?php echo "Pokemon" ?></h2>
         <table>
             <tr>
-                <th>Code</th>
+                <th>ID</th>
                 <th>Name</th>
-                <th class="right">Price</th>
+                <th>Type 1</th>
+                <th>Type 2</th>
+                <th>HP</th>
+                <th>Attack</th>
+                <th>Defense</th>
+                <th>Sp. Attack</th>
+                <th>Sp. Defense</th>
+                <th>Speed</th>
+                <th class="right">Stat Total</th>
             </tr>
 
-            <?php foreach ($products as $product) : ?>
+            <?php foreach ($Pokedex as $pokemon) : ?>
             <tr>
-                <td><?php echo $product['productCode']; ?></td>
-                <td><?php echo $product['productName']; ?></td>
-                <td class="right"><?php echo $product['listPrice']; ?></td>
+                <td><?php echo $pokemon['id']; ?></td>
+                <td><?php echo $pokemon['P_Name']; ?></td>
+                <td><?php echo $pokemon['typeName']; ?></td>
+                <td><?php echo $pokemon['type2']; ?></td>
+                <td><?php echo $pokemon['HP']; ?></td>
+                <td><?php echo $pokemon['Attack']; ?></td>
+                <td><?php echo $pokemon['Defense']; ?></td>
+                <td><?php echo $pokemon['Sp_Attack']; ?></td>
+                <td><?php echo $pokemon['Sp_Defense']; ?></td>
+                <td><?php echo $pokemon['Speed']; ?></td>
+                <td class="right"><?php echo $pokemon['stat_total']; ?></td>
             </tr>
             <?php endforeach; ?>
         </table>
